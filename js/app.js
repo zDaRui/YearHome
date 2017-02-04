453a16e198133551ca538216d0c7c4038834d7e7
zDaRui <184003018@qq.com>
2017-02-04 16:44:05 +0800

    2017

---
+++ js/app.js
@@ -0,0 +1,7 @@
+/**
+ * Created by ´óÈð on 2017/1/13.
+ */
+angular.module('myApp',[
+    'ionic',
+    'ui.router'
+])
---
+++ js/controller.js
@@ -0,0 +1,5 @@
+/**
+ * Created by ´óÈð on 2017/1/13.
+ */
+angular.module('myApp')
+    .controller('dirCtrl', dirCtrl)
